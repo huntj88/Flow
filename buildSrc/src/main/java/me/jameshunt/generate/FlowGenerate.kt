cac02805eb07e54977c581f0677440d1dfdfe1f3
huntj88 <huntj88@gmail.com>
2019-04-30 00:21:10 -0500

    example of code generation stuff

--- buildSrc/src/main/java/me/jameshunt/generate/FlowGenerate.kt
+++ buildSrc/src/main/java/me/jameshunt/generate/FlowGenerate.kt
@@ -9,7 +9,8 @@ object FlowGenerate {
     fun generate() {
         println("sup")
 
-        val file = File("${Paths.get("").toAbsolutePath()}/app/src/main/java/me/jameshunt/flow3/summary/Summary.puml")
+        val file = File("${Paths.get("").toAbsolutePath()}/app/src/main/java/me/jameshunt/flow3/summary/PillReminder.puml")
+//        val file = File("${Paths.get("").toAbsolutePath()}/app/src/main/java/me/jameshunt/flow3/summary/Summary.puml")
 
         FlowGenerator(file).generate()
     }
