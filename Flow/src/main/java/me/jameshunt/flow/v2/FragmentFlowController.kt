ea0d6fb3346dde3b813ef791cede648d3388b07b
huntj88 <huntj88@gmail.com>
2019-06-12 17:57:44 -0500

    progress on better separation of concerns

--- Flow/src/main/java/me/jameshunt/flow/v2/FragmentFlowController.kt
+++ Flow/src/main/java/me/jameshunt/flow/v2/FragmentFlowController.kt
@@ -1,9 +1,16 @@
-package me.jameshunt.flow
+package me.jameshunt.flow.v2
 
 import com.inmotionsoftware.promisekt.Promise
 import com.inmotionsoftware.promisekt.ensure
-
-typealias ViewId = Int
+import com.inmotionsoftware.promisekt.recover
+import com.inmotionsoftware.promisekt.thenMap
+import me.jameshunt.flow.FlowDialogFragment
+import me.jameshunt.flow.FlowFragment
+import me.jameshunt.flow.FlowManager
+import me.jameshunt.flow.FlowResult
+import me.jameshunt.flow.FlowUI
+import me.jameshunt.flow.FragmentProxy
+import me.jameshunt.flow.ViewId
 
 interface AndroidFlowFunctions {
     fun <NewInput, NewOutput, Controller> flow(
@@ -19,11 +26,15 @@ interface AndroidFlowFunctions {
             where FragmentType : FlowUI<FragInput, FragOutput>
 }
 
-abstract class FragmentFlowController<Input, Output> : FlowController<Input, Output>() {
+abstract class FragmentFlowController<Input, Output> : FlowController<Input, FlowResult<Output>>() {
+
+    interface BackState
 
     interface DoneState<Output> {
         val output: Output
     }
+
+    protected lateinit var currentState: State
 
     internal var viewId: ViewId = 0 // is only set once at the beginning
 
@@ -50,12 +61,34 @@ abstract class FragmentFlowController<Input, Output> : FlowController<Input, Out
         return flowFunctions.flow(controller = controller, input = input)
     }
 
+    // Inlining this gives better errors about where the error happened
+    inline fun <Result, From> Promise<FlowResult<Result>>.forResult(
+        crossinline onBack: () -> Promise<From> = { throw NotImplementedError("onBack") },
+        crossinline onComplete: (Result) -> Promise<From> = { throw NotImplementedError("onComplete") },
+        crossinline onCatch: ((Throwable) -> Promise<From>) = { throw it }
+    ): Promise<From> = this
+        .thenMap {
+            when (it) {
+                is FlowResult.Back -> onBack()
+                is FlowResult.Completed -> onComplete(it.data)
+            }
+        }
+        .recover { onCatch(it) }
+
+    // internal to this instance use
+    final override fun launchFlow(input: Input): Promise<FlowResult<Output>> {
+        currentState = InitialState(input)
+        return super.launchFlow(input)
+    }
+
     /**
      * resuming only renders the view again
      * the fragment reattaches itself to the existing promise
      */
 
-    final override fun resume(currentState: State) {
+    internal fun resume() = resume(currentState)
+
+    private fun resume(currentState: State) {
         (activeFragment as? FragmentProxy<Any?, Any?, FlowFragment<Any?, Any?>>)?.let {
             FlowManager.fragmentDisplayManager.show(
                 fragmentProxy = it,
@@ -77,12 +110,19 @@ abstract class FragmentFlowController<Input, Output> : FlowController<Input, Out
 
     fun DoneState<Output>.onDone() {
         FlowManager.fragmentDisplayManager.remove(activeFragment)
-        super.onDone(output)
+        super.onDone(FlowResult.Completed(output))
     }
 
-    final override fun handleBack() {
+    protected fun BackState.onBack() {
+        super.onDone(FlowResult.Back)
+    }
+
+    internal fun handleBack() {
         // does not call FlowController.onBack() ever. that must be done explicitly with a state transition
-        this.childFlows.firstOrNull()?.handleBack() ?: this.activeFragment?.onBack()
+        this.childFlows.firstOrNull()
+            ?.let { it as? FragmentFlowController<*, *> }
+            ?.handleBack()
+            ?: this.activeFragment?.onBack()
     }
 
     inner class AndroidFlowFunctionsImpl : AndroidFlowFunctions {
@@ -94,7 +134,7 @@ abstract class FragmentFlowController<Input, Output> : FlowController<Input, Out
             val flowController = controller.newInstance().apply {
                 // apply same viewId to child
                 this@apply.viewId = this@FragmentFlowController.viewId
-            }
+            } as FlowController<NewInput, FlowResult<NewOutput>>
 
             childFlows.add(flowController)
 
