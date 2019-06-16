f10cddd1e4d339d61beb0296a55c88ed48343e55
huntj88 <huntj88@gmail.com>
2019-06-16 12:18:22 -0500

    BackgroundTask with extensions to automatically put you on a background executor

--- Flow/src/main/java/me/jameshunt/flow/BusinessFlowController.kt
+++ Flow/src/main/java/me/jameshunt/flow/BusinessFlowController.kt
@@ -1,8 +1,6 @@
 package me.jameshunt.flow
 
-import com.inmotionsoftware.promisekt.*
-import java.util.concurrent.Executor
-import java.util.concurrent.Executors
+import com.inmotionsoftware.promisekt.Promise
 
 interface BusinessFlowFunctions {
     fun <NewInput, NewOutput, Controller> flow(
@@ -12,7 +10,7 @@ interface BusinessFlowFunctions {
             where Controller : BusinessFlowController<NewInput, NewOutput>
 }
 
-abstract class BusinessFlowController<Input, Output> : FlowController<Input, Output>() {
+abstract class BusinessFlowController<Input, Output> : FlowController<Input, Output>(), BackgroundTask {
 
     private var flowFunctions: BusinessFlowFunctions = BusinessFlowFunctionsImpl()
 
@@ -38,37 +36,4 @@ abstract class BusinessFlowController<Input, Output> : FlowController<Input, Out
             }
         }
     }
-
-    /**
-     * operate on background thread by default
-     */
-
-    companion object {
-        private val backgroundExecutor: Executor? = Executors.newCachedThreadPool()
-    }
-
-    fun <T, U> Promise<T>.map(map: (T) -> U): Promise<U> =
-        this.map(on = backgroundExecutor) { map(it) }
-
-    fun <T> Promise<T>.ensure(ensure: () -> Unit): Promise<T> =
-        this.ensure(on = backgroundExecutor) { ensure() }
-
-    fun <T> Promise<T>.done(done: () -> Unit): Promise<Unit> =
-        this.done(on = backgroundExecutor) { done() }
-
-    fun <T> Promise<T>.then(then: (T) -> Promise<T>): Promise<T> =
-        this.then(on = backgroundExecutor) { then(it) }
-
-    fun <T, U> Promise<T>.thenMap(thenMap: (T) -> Promise<U>): Promise<U> =
-        this.thenMap(on = backgroundExecutor) { thenMap(it) }
-
-    fun <T> Promise<T>.recover(recover: (Throwable) -> Promise<T>): Promise<T> =
-        this.recover(on = backgroundExecutor) { recover(it) }
-
-    fun <T> Promise<T>.catch(catch: (Throwable) -> Unit): PMKFinalizer =
-        this.catch(on = backgroundExecutor) { catch(it) }
-
-    fun PMKFinalizer.finally(finally: () -> Unit) =
-        this.finally(on = backgroundExecutor) { finally() }
-
 }
