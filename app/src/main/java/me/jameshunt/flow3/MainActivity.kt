47f83cdef1615cd70fb9a4204dce91a6439de618
huntj88 <huntj88@gmail.com>
2020-02-15 20:53:06 -0600

    basic docs

--- app/src/main/java/me/jameshunt/flow3/MainActivity.kt
+++
@@ -1,8 +0,0 @@
-package me.jameshunt.flow3
-
-import me.jameshunt.flow.FlowActivity
-import me.jameshunt.flow3.splash.SplashFlowController
-
-class MainActivity : FlowActivity<SplashFlowController>() {
-    override fun getInitialFlow(): Class<SplashFlowController> = SplashFlowController::class.java
-}
